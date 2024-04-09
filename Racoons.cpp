a0a6c255eadb9cd8e126a7f7a28f48d7535c0ed7
Jose Vigil <vigiljosereynaldo@gmail.com>
2024-04-09 18:46:28 -0500

    added strcmp

--- Racoons.cpp
+++ Racoons.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main ()
 {
-    int ask;
-    char c_string[] = "abc";
+    char c_string_1[] = "Hello";
+    char c_string_2[] = "World";
+
+    if(strcmp(c_string_2, c_string_1))
+    cout << "True";
+
     
 }
