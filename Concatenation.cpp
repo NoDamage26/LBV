a1a0260d475bcc244dd5323f7f5214f4fc2213a3
Jose Vigil <vigiljosereynaldo@gmail.com>
2024-04-09 18:59:53 -0500

    added Concatenation.cpp

---
+++ Concatenation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char c_string_1[20] = "The Rain";
+    char c_string_2[] = "do you remember";
+    cout << "Before Concatenation: " << c_string_1 << endl;
+
+
+    //Concatenation will Join 2 strings into one string
+
+
+    cout << "After Concatenation: "; 
+}
